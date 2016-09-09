57fa944205b96bc40862d8b15fe64699897c2ffe
moeh1234 <moeh1234@gmail.com>
2016-09-09 13:17:19 -0400

    Lab 3 is done

--- Lab3/Program.cs
+++ Lab3/Program.cs
@@ -6,10 +6,37 @@ using System.Threading.Tasks;
 
 namespace Lab3
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-        }
+            Console.WriteLine("Learn your squares and cubes!\n");
+            string DOYOUWANTTOCONTINUE;
+            do
+            {
+                Console.Write("Enter an integer:\n");
+                string Input = Console.ReadLine();
+                Console.WriteLine();
+
+                
+                    Console.WriteLine("Number   Squared     Cubed");
+                    Console.WriteLine("=======  =======     ======");
+                    for (int value = 1; value <= int.Parse(Input); value++)
+
+                    {
+                        int squared = (int)Math.Pow(value, 2);
+                        int cubed = (int)Math.Pow(value, 3);
+                        Console.WriteLine("{0}        {1:N0}           {2:N0}",
+                                            value, squared, cubed);
+                    }
+                    Console.WriteLine();
+                    Console.Write("Continue? (y/n): ");
+                    DOYOUWANTTOCONTINUE = Console.ReadLine();
+                    Console.WriteLine();
+
+
+            } while (DOYOUWANTTOCONTINUE == "y");
+            Console.WriteLine("Goodbye!");
+         }
     }
 }
